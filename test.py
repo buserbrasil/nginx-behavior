e1ea10c25e4f7abb2ca11182db4c535379512ab0
Iuri de Silvio <iurisilvio@gmail.com>
2022-05-15 16:15:24 -0300

    Test proxy_valid_cache, cookies request and set-cookies response.

--- test.py
+++ test.py
@@ -3,8 +3,16 @@ import requests
 BASE_URL = 'http://localhost:8080'
 
 
-def _get(path, params=None):
-    return requests.get(f'{BASE_URL}{path}', params=params)
+def _get(path, params=None, *, cookies=None):
+    return requests.get(f'{BASE_URL}{path}', params=params, cookies=cookies)
+
+
+def assert_miss(response):
+    assert response.headers['X-Cache-Status'] in {'MISS', 'EXPIRED'}
+
+
+def assert_hit(response):
+    assert response.headers['X-Cache-Status'] == 'HIT'
 
 
 def test_nginx_no_cache_header():
@@ -12,7 +20,7 @@ def test_nginx_no_cache_header():
     response = _get(path)
     response = _get(path)
     assert 'Cache-Control' not in response.headers
-    assert response.headers['X-Cache-Status'] == 'MISS'
+    assert_miss(response)
 
 
 def test_nginx_respects_cache_control_max_age():
@@ -22,10 +30,10 @@ def test_nginx_respects_cache_control_max_age():
     }
 
     response = _get(path, params)
-    assert response.headers['X-Cache-Status'] in {'MISS', 'EXPIRED'}
+    assert_miss(response)
 
     response = _get(path, params)
-    assert response.headers['X-Cache-Status'] == 'HIT'
+    assert_hit(response)
 
 
 def test_nginx_respects_cache_control_s_maxage():
@@ -35,10 +43,10 @@ def test_nginx_respects_cache_control_s_maxage():
     }
 
     response = _get(path, params)
-    assert response.headers['X-Cache-Status'] in {'MISS', 'EXPIRED'}
+    assert_miss(response)
 
     response = _get(path, params)
-    assert response.headers['X-Cache-Status'] == 'HIT'
+    assert_hit(response)
 
 
 def test_nginx_respects_cache_control_prefer_s_maxage():
@@ -48,10 +56,10 @@ def test_nginx_respects_cache_control_prefer_s_maxage():
     }
 
     response = _get(path, params)
-    assert response.headers['X-Cache-Status'] in {'MISS', 'EXPIRED'}
+    assert_miss(response)
 
     response = _get(path, params)
-    assert response.headers['X-Cache-Status'] == 'HIT'
+    assert_hit(response)
 
 
 def test_nginx_respects_cache_control_prefer_s_maxage_zero():
@@ -61,7 +69,75 @@ def test_nginx_respects_cache_control_prefer_s_maxage_zero():
     }
 
     response = _get(path, params)
-    assert response.headers['X-Cache-Status'] == 'MISS'
+    assert_miss(response)
 
     response = _get(path, params)
-    assert response.headers['X-Cache-Status'] == 'MISS'
+    assert_miss(response)
+
+
+def test_nginx_proxy_cache_valid():
+    path = '/proxy-cache-valid/status/200'
+
+    response = _get(path)
+    assert_miss(response)
+
+    response = _get(path)
+    assert_hit(response)
+
+
+def test_nginx_proxy_cache_valid_ignored_when_max_age_is_defined():
+    path = '/proxy-cache-valid/response-headers'
+    params = {
+        'Cache-Control': 'max-age=0',
+    }
+
+    response = _get(path, params)
+    assert_miss(response)
+
+    response = _get(path, params)
+    assert_miss(response)
+
+
+def test_nginx_proxy_cache_valid_ignored_when_s_maxage_is_defined():
+    path = '/proxy-cache-valid/response-headers'
+    params = {
+        'Cache-Control': 's-maxage=0',
+    }
+
+    response = _get(path, params)
+    assert_miss(response)
+
+    response = _get(path, params)
+    assert_miss(response)
+
+
+def test_nginx_cache_with_cookies_in_request():
+    path = '/response-headers'
+    params = {
+        'Cache-Control': 'max-age=10',
+    }
+    cookies = {
+        'foo': 'bar',
+    }
+
+    response = _get(path, params, cookies=cookies)
+    assert_miss(response)
+
+    response = _get(path, params, cookies=cookies)
+    assert_hit(response)
+
+
+def test_nginx_never_cache_with_set_cookie_in_response():
+    path = '/proxy-cache-valid/cookies/set'
+    params = {
+        'Cache-Control': 'max-age=10',
+    }
+    cookies = {
+        'foo': 'bar',
+    }
+
+    response = _get(path, params, cookies=cookies)
+    assert_miss(response)
+
+    response = _get(path, params, cookies=cookies)
+    assert_miss(response)
